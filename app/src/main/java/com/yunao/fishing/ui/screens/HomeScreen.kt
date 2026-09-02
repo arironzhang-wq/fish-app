ee429ec026b6aafba1e95af23ed8b37f06cfb135
arironzhang-wq <arironzhang@gmail.com>
2026-09-02 12:28:09 -0400

    Add import for width in HomeScreen.kt

--- app/src/main/java/com/yunao/fishing/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/yunao/fishing/ui/screens/HomeScreen.kt
@@ -1,4 +1,5 @@
 package com.yunao.fishing.ui.screens
+import androidx.compose.foundation.layout.width
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
